400ffb2d37f6a036aefdf3517a95dd5c0e389d99
Antonio Tafra <antonio.tafra@gmail.com>
2021-02-09 18:23:27 +0100

    CRUD operations exercise on a Videos domain model

--- Vidzy/Vidzy/Program.cs
+++ Vidzy/Vidzy/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Vidzy.DAL;
 using Vidzy.Models;
 
@@ -13,19 +14,30 @@ namespace Vidzy
 
             using(var context = new VidzyDbContext())
             {
-                var videos = new List<Video>
-                {
-                    new Video { Name = "Star Wars", ReleaseTime = DateTime.Now },
-                    new Video { Name = "Troy", ReleaseTime = DateTime.Now }
-                };
+                //var videos = new List<Video>
+                //{
+                //    new Video { Name = "Star Wars", ReleaseTime = DateTime.Now, Classification = Classification.Gold },
+                //    new Video { Name = "Troy", ReleaseTime = DateTime.Now, Classification = Classification.Platinum }
+                //};
 
+                //foreach (var video in videos)
+                //{
+                //    context.Videos.Add(video);
+                //}
 
+                var videos = context.Videos.ToList();
 
+                //foreach (var video in videos)
+                //{
+                //  context.Videos.Remove(video);
+                //}
+
+                videos[0].Classification = Classification.Platinum;
+                videos[1].Classification = Classification.Gold;
                 foreach (var video in videos)
                 {
-                    context.Videos.Add(video);
+                    context.Videos.Update(video);
                 }
-
                 context.SaveChanges();
 
 
